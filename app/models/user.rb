8ee1d5cfb3b30680257005861fc324893c379e5d
EC2 Default User <ec2-user@ip-172-31-42-82.ap-northeast-1.compute.internal>
2022-01-24 08:34:55 +0000

    fix Session Controller and more

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :authentication_keys => [:name]
-         
+
   has_many :books, dependent: :destroy
   attachment :profile_image
-  
+
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }, uniqueness: false
   validates_uniqueness_of :name
@@ -17,10 +17,10 @@ class User < ApplicationRecord
   # 登録時にemailを不要とする
   def email_required?
     false
-  end 
+  end
 
   def email_changed?
     false
-  end 
+  end
 
 end
